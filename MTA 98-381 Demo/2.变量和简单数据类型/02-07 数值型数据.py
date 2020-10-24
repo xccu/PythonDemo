cb043b74e63f56e83e2f7255c88600839de536b6
xccu <264672790@qq.com>
2020-10-24 10:57:15 +0800

    added chapter3

---
+++ MTA 98-381 Demo/2.变量和简单数据类型/02-07 数值型数据.py
@@ -0,0 +1,22 @@
+#整型变量 int
+x=3
+#浮点型变量 float
+y=1.5
+print(x+y)
+print(y-x)
+print(x*y)
+#所有除法结果都是浮点数
+print(x/y) 
+#乘方 二的六次方
+print(2**6)  
+#取负值
+print(-x)
+#余数除法取余
+print(10%3)
+#余数除法取商
+print(10//3)
+#复杂运算
+print((3+2)/2)
+#10的100次方
+print(10**100)
+print(0.1+0.2)
---
+++ MTA 98-381 Demo/3.数据类型转换和数值计算函数/03-01 判断和转换数据类型.py
@@ -0,0 +1,20 @@
+x=5
+y=3.14
+z='007'
+#打印 <class 'int'> <class 'float'> <class 'str'>
+print(type(x),type(y),type(z))
+
+a=True
+b=False
+#打印 <class 'bool'> <class 'bool'>
+print(type(a),type(b))
+#int转float
+print(float(x))
+#float转int 小数点后舍去（不做四舍五入）
+print(int(y),int(3.54))
+#str转int 打印 7
+print(int(z))
+#报错 
+#print(int('aaa'))
+#其他类型转str 打印
+print(str(x),str(y),type(str(x)))
