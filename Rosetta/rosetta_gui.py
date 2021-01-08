3509edec3543a8fce516fdb895d9ed08ad552f3e
xccu <264672790@qq.com>
2021-01-08 21:05:18 +0800

    modified Roestta

--- Rosetta/rosetta_gui.py
+++ Rosetta/rosetta_gui.py
@@ -7,6 +7,7 @@ class Init_Window():
     #构造函数
     def __init__(self,window):
         self.window = window
+        self.rsa = RSA_Util()
 
     def init(self):
         #设置窗体title
@@ -20,13 +21,9 @@ class Init_Window():
         #self.init_Label.grid(row=0, column=0)
         self.init_Label.place(x=10,y=10)
 
-        #创建文本框
-        self.init_Text = Text(self.window, width=50, height=10)
-        self.init_Text.place(x=10,y=50)
-
-        #创建加密文本框
-        self.encrypt_Text = Text(self.window, width=50, height=10)
-        self.encrypt_Text.place(x=400,y=50)
+        #创建日志文本框
+        self.log_Text = Text(self.window, width=100, height=10)
+        self.log_Text.place(x=10,y=50)
 
         #加密按钮
         self.encrypt_button = Button(self.window, text="加密", width=10,command=self.encrypt_click)  # 调用内部方法  加()为直接调用
@@ -36,9 +33,13 @@ class Init_Window():
         self.decrypt_button = Button(self.window, text="解密", width=10,command=self.decrypt_click)  # 调用内部方法  加()为直接调用
         self.decrypt_button.place(x=100,y=200)
 
-        #保存按钮
-        self.decrypt_button = Button(self.window, text="保存", width=10,command=self.save_file_click)  # 调用内部方法  加()为直接调用
-        self.decrypt_button.place(x=190,y=200)
+        #生成密钥按钮
+        self.create_key_button = Button(self.window, text="生成密钥", width=10,command=self.create_keys_click)  # 调用内部方法  加()为直接调用
+        self.create_key_button.place(x=190,y=200)
+
+        #导入私钥按钮
+        self.import_key_button = Button(self.window, text="导入私钥", width=10)  # 调用内部方法  加()为直接调用
+        self.import_key_button.place(x=280,y=200)
 
         #选择文件按钮
         self.open_file_button = Button(self.window, text="选择文件", width=10,command=self.open_file_click)  # 调用内部方法  加()为直接调用
@@ -50,39 +51,29 @@ class Init_Window():
 
     #加密函数
     def encrypt_click(self):
-        #清空文本控件
-        self.encrypt_Text.delete('1.0','end')
-        #获取文本控件内容
-        data = self.init_Text.get("0.0", "end")
-        aes = AES_Util()
-        self.databyte=aes.encrypt(data)
-        encrypt_data = "{}".format(self.databyte)
-        self.encrypt_Text.insert(1.0,encrypt_data)
+        self.rsa.encrypt(self.filePath)
+        self.log_Text.insert(1.0,'已加密：'+self.filePath+'\n')
 
     #解密函数
     def decrypt_click(self):
-        #清空文本控件
-        self.encrypt_Text.delete('1.0','end')
-        aes = AES_Util()
-        dataStr=aes.decrypt(self.databyte)
-        encrypt_data = "{}".format(dataStr)
-        self.encrypt_Text.insert(1.0,encrypt_data)
+        self.rsa.decrypt(self.filePath)
+        self.log_Text.insert(1.0,'已解密：'+self.filePath+'\n')
 
     #打开文件函数
     def open_file_click(self):
         #self.window.withdraw()
+
+         #清空文本控件
+        self.file_Text.delete('1.0','end')
         # 选择文件
         self.filePath = filedialog.askopenfilename()
         # 显示文件路径
         self.file_Text.insert(1.0,self.filePath)
-        fileUtil=Flie_Util()
-        data = fileUtil.read(self.filePath)
-        self.init_Text.insert(1.0,data)
+        #fileUtil=Flie_Util()
+        #data = fileUtil.read(self.filePath)
+        #self.init_Text.insert(1.0,data)
 
     #保存文件函数
-    def save_file_click(self):
-        #self.window.withdraw()
-
-        self.filePath=self.filePath.replace('.txt','.rosetta')
-        fileUtil=Flie_Util()
-        fileUtil.writeBytes(self.filePath,self.databyte)
+    def create_keys_click(self):
+        self.rsa.create_rsa_key()
+        self.log_Text.insert(1.0,'已生成密钥'+'\n')
