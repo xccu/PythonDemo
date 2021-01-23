33ad753cca4e89de07115a64e73789e7770f8a1c
xccu <264672790@qq.com>
2021-01-23 14:13:03 +0800

    added thread-write-demo.py

---
+++ thread-write-demo.py
@@ -0,0 +1,39 @@
+import threading
+import _thread
+import time
+
+threadLock = threading.Lock()
+dict_data={}
+
+def write_string(no,path="test.txt"):
+    time.sleep(5)
+    count=0
+    i=0
+    while i <= no:
+        count += (i)
+        i+=1
+    print(str(no)+":\t"+str(count))
+    dict_data[no] = str(no)+":\t"+str(count)+'\n'
+
+# 创建新线程
+print('开始')
+threads = []
+for i in range(1,1001):
+    #_thread.start_new_thread(self.encrypt_folder_thread,())
+    t = threading.Thread(target=write_string, args=[i])
+    t.start()
+    threads.append(t)
+
+
+for t in threads:
+    t.join()
+ 
+print("所有线程任务完成")
+
+#按照字典的key排序输出
+list_data=[]
+for i in sorted (dict_data) : 
+    list_data.append(dict_data[i])
+
+with open('test.txt', 'a') as f:
+    f.writelines(list_data )
